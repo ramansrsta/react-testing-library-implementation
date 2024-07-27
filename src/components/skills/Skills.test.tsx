669a616d89059fea5bc3977c1c071a6489af1a05
rmnsrsta <ramanshrestha@varicon.com.au>
2024-07-27 12:03:36 +0545

    Added Findy and query by tests

--- src/components/skills/Skills.test.tsx
+++ src/components/skills/Skills.test.tsx
@@ -15,4 +15,34 @@ describe("Skills", () => {
     const listItems = screen.getAllByRole("listitem");
     expect(listItems).toHaveLength(skills.length);
   });
+
+  test("renders login button", () => {
+    render(<Skills skills={skills} />);
+    const loginButton = screen.getByRole("button", { name: "Login" });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  test("start learning button is not rendered", () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = screen.queryByRole("button", {
+      name: "Start learning",
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
+  });
+
+  test("Start Learning Button Is Eventually Rendered", async () => {
+    render(<Skills skills={skills} />);
+    // screen.debug();
+    const startLearningButton = await screen.findByRole(
+      "button",
+      {
+        name: "Start learning",
+      },
+      {
+        timeout: 2000,
+      }
+    );
+    // screen.debug();
+    expect(startLearningButton).toBeInTheDocument();
+  });
 });
