82ac6ef8d4e8c2a3e0c49b0890af9c53cad610bd
rmnsrsta <ramanshrestha@varicon.com.au>
2024-07-27 16:21:07 +0545

    Added Custom Render Fn and Test For Custom Hook

--- src/components/mui/MuiMode.test.tsx
+++ src/components/mui/MuiMode.test.tsx
@@ -1,13 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../test-utils";
 
 import { MuiMode } from "./MuiMode";
-import { AppProviders } from "../../providers/AppProviders";
 
 describe("Mui Mode", () => {
   test("renders text correctly", () => {
-    render(<MuiMode />, {
-      wrapper: AppProviders,
-    });
+    render(<MuiMode />);
     const headingElement = screen.getByRole("heading");
     expect(headingElement).toHaveTextContent("dark mode");
   });
