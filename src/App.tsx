cfb2f835383f3ff10772924ea83047fe7b47fa6e
rmnsrsta <ramanshrestha@varicon.com.au>
2024-07-28 15:15:08 +0545

    husky added'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { AppProviders } from "./providers/AppProviders";
 import { MuiMode } from "./components/mui/MuiMode";
+import { Application } from "./components/application/Application";
 
 function App() {
   return (
     <AppProviders>
       <div className="App">
         <MuiMode />
+        <Application />
       </div>
     </AppProviders>
   );
